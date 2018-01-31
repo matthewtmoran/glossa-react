9b3af5780b5ab48a6657245cf4f01ccaa62fa3a7
matthewtmoran <matthewtmoran@gmail.com>
2018-01-31 17:04:26 -0600

    created transcription node/electron api

---
+++ src/server/api/transcription/index.js
@@ -0,0 +1,67 @@
+import TranscriptionPouch from '../../pouch/transcription';
+// here we define all our functions and pass them to client
+//the response we return should be serialized and identical to that of the express controller
+//here we connect to the database directly
+
+const {
+  find,
+  create,
+  remove,
+  update,
+  updateOrCreate
+} = TranscriptionPouch;
+
+const fetchTranscriptionsAPI = () => {
+  return new Promise((resolve, reject) => {
+    find()
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((reason) => {
+        reject(reason)
+      })
+  })
+};
+
+const updateTranscriptionAPI = (data) => {
+  return new Promise((resolve, reject) => {
+    update(data)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((reason) => {
+        reject(reason)
+      })
+  })
+};
+
+const createTranscriptionAPI = (data) => {
+  return new Promise((resolve, reject) => {
+    create(data)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((reason) => {
+        reject(reason)
+      })
+  })
+};
+
+const removeTranscriptionAPI = (data) => {
+  return new Promise((resolve, reject) => {
+    remove(data)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((reason) => {
+        reject(reason)
+      })
+  })
+};
+
+export {
+  fetchTranscriptionsAPI,
+  updateTranscriptionAPI,
+  createTranscriptionAPI,
+  removeTranscriptionAPI,
+}
